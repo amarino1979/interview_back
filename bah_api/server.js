ac478d1f15a069d70ae7965ce8b2ac174482c882
Aaron Marino <aaron_marino@yahoo.com>
2020-09-02 19:35:59 -0400

    added cors to server.js

--- bah_api/server.js
+++ bah_api/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
+const cors = require('cors')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 const port = 3000
@@ -24,6 +25,18 @@ mongoose.connection.once('open', () => {
 // middleware
 app.use(express.json()) //use .json(), not urlencoded()
 
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        }else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 // Router Controller //
 const clientsController = require('./controllers/clients.js')
 app.use('/clients', clientsController)
