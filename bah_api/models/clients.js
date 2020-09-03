c648873c790f8938558028e924eb6ea5bc6c8740
Aaron Marino <aaron_marino@yahoo.com>
2020-09-03 18:49:16 -0400

    updated schema and added blank-profile.png

--- bah_api/models/clients.js
+++ bah_api/models/clients.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 const Client = mongoose.Schema({
-    name: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String},
+    city: {type: String},
+    state: {type: String},
+    image: {data: Buffer, contentType: String},
     rank: {type: String, required: true},
     timeStamp: {type: Date, default: Date.now},
 })
